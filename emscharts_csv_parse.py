6f7f36fb96a356fdcb7a614cc7eae9487ca7f682
unratedpupet <zphillips.de@gmail.com>
2023-05-22 01:02:06 -0400

    Added environment Changed df index to the PRID

--- emscharts_csv_parse.py
+++ emscharts_csv_parse.py
@@ -1,11 +1,14 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 file_path = 'XML_files/emscharts/YTD_5-21-23_emsCharts_CARES_Report.csv'
 
 data = pd.read_csv(file_path)
 
 df = pd.DataFrame(data)
+pd.options.display.width = 10
+
+print(df.head())
+
+df = df.set_index("PRID")
 
 print(df.head())
